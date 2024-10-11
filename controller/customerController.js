88a882b9e1a1c3c387083c4e645cdab02a4b5735
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-10-11 23:06:25 +0530

    customer get all function implement using ajax.

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,27 +1,75 @@
-import { saveCustomer } from "../model/customerModel.js";
+import { saveCustomer, getAllCustomers } from "../model/customerModel.js";
 
-$(document).ready(function () {
-  $("#add-customer-btn").click(function () {
-    const firstName = $("#first-name").val();
-    const lastName = $("#last-name").val();
-    const address = $("#address").val();
-    const mobile = $("#mobile").val();
-    const email = $("#email").val();
-
-    const customerData = {
-      firstName: firstName,
-      lastName: lastName,
-      address: address,
-      mobile: mobile,
-      email: email,
-    };
-
-    saveCustomer(customerData)
-      .done(function (result) {
-        swal("Confirmation!", "Customer Saved Successfully!", "success");
-      })
-      .fail(function (error) {
-        swal("Error!", "Customer Save Failed!", "error");
-      });
-  });
+$(document).ready(async function () {
+  try {
+    const customerList = await getAllCustomers();
+    loadCustomerTable(customerList);
+    loadCustomerId(customerList.map((customers) => customers.customerId));
+  } catch (error) {
+    console.error("Error fetching customers:", error);
+  }
 });
+
+$("#add-customer-btn").click(function () {
+  const firstName = $("#first-name").val();
+  const lastName = $("#last-name").val();
+  const address = $("#address").val();
+  const mobile = $("#mobile").val();
+  const email = $("#email").val();
+
+  const customerData = {
+    firstName: firstName,
+    lastName: lastName,
+    address: address,
+    mobile: mobile,
+    email: email,
+  };
+
+  saveCustomer(customerData)
+    .done(function (result) {
+      swal("Confirmation!", "Customer Saved Successfully!", "success");
+    })
+    .fail(function (error) {
+      swal("Error!", "Customer Save Failed!", "error");
+    });
+});
+
+function loadCustomerTable(customerList) {
+  $("#customer-table").empty();
+  customerList.forEach(function (customer) {
+    $(".table").append(
+      "<tr> " +
+        "<td>" +
+        customer.customerId +
+        "</td>" +
+        "<td>" +
+        customer.name +
+        "</td>" +
+        "<td>" +
+        customer.address +
+        "</td>" +
+        "<td>" +
+        customer.email +
+        "</td>" +
+        "<td>" +
+        customer.mobile +
+        "</td>" +
+        "<td>" +
+        customer.lastUpdatedAt +
+        "</td>" +
+        "</tr>"
+    );
+  });
+}
+
+function loadCustomerId(customerIds) {
+  const customerId = $("#customer-id");
+  customerId.empty();
+  customerId.append('<option value="">Search,Update or Delete Customer</option>');
+
+  customerIds.forEach(function (id) {
+    customerId.append(
+        `<option value="${id}">${id}</option>`
+    )
+  })
+}
--- model/customerModel.js
+++ model/customerModel.js
@@ -1,8 +1,24 @@
-export function saveCustomer(customerData) {
+export async function getAllCustomers() {
+    return $.ajax({
+      url: "http://localhost:8080/Spring_POS_API/api/v1/customers",
+      method: "GET",
+      dataType: "json",
+
+      success: function(customerList) {            
+        return customerList;
+      },
+
+      error: function (error) {
+        console.log(error);
+      }
+    });
+}
+
+export function saveCustomer(customer) {
   return $.ajax({
     url: "http://localhost:8080/Spring_POS_API/api/v1/customers",
     method: "POST",
     contentType: "application/json",
-    data: JSON.stringify(customerData),
+    data: JSON.stringify(customer),
   });
 }
