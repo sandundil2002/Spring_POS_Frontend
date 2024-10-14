4764175ffa55ca71e15868017522d41eb3055825
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-10-14 21:33:54 +0530

    item save function implement using ajax.

--- controller/itemController.js
+++ controller/itemController.js
@@ -1,7 +1,29 @@
-import { getAllItems } from "../model/itemModel.js";
+import { getAllItems, saveItem } from "../model/itemModel.js";
 
 $(document).ready(function () {
   reloadItems();
+});
+
+$("#add-item-btn").click(function () {
+  const category = $("#item-category").val();
+  const unitPrice = $("#price").val();
+  const qtyOnHand = $("#qty").val();
+  const expireDate = $("#ex-date").val();
+
+  const itemData = {
+    category: category,
+    unitPrice: unitPrice,
+    qtyOnHand: qtyOnHand,
+    expireDate: expireDate,
+  };
+
+  console.log(itemData);
+  
+
+  const promise = saveItem(itemData);
+  promise.then(() => {
+    reloadItems();
+  });
 });
 
 function loadItemTable(itemList) {
@@ -33,15 +55,13 @@ function loadItemTable(itemList) {
 }
 
 function loadItemCode(itemCodes) {
-    const itemCode = $("#item-code");
-    itemCode.empty();
-    itemCode.append(
-      '<option value="">Search,Update or Delete Item</option>'
-    );
+  const itemCode = $("#item-code");
+  itemCode.empty();
+  itemCode.append('<option value="">Search,Update or Delete Item</option>');
 
-    itemCodes.forEach(function (code) {
-        itemCode.append(`<option value="${code}">${code}</option>`);
-    })
+  itemCodes.forEach(function (code) {
+    itemCode.append(`<option value="${code}">${code}</option>`);
+  });
 }
 
 async function reloadItems() {
@@ -51,7 +71,7 @@ async function reloadItems() {
     $("#ex-date").val("");
     const itemList = await getAllItems();
     loadItemTable(itemList);
-    loadItemCode(itemList.map(item => item.itemCode)); 
+    loadItemCode(itemList.map((item) => item.itemCode));
   } catch (error) {
     console.error("Error fetching items:", error);
   }
--- model/itemModel.js
+++ model/itemModel.js
@@ -9,3 +9,20 @@ export async function getAllItems() {
       },
     });  
 }
+
+export function saveItem(item) {
+  return $.ajax({
+    url: "http://localhost:8080/Spring_POS_API/api/v1/items",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(item),
+
+    success: function () {
+      swal("Confirmation!", "Item Saved Successfully!", "success");
+    },
+
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
